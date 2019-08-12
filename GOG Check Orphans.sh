aa24721ee167d02cbe506dc9f9d76c55c8273ba7
TheFrenchGhosty <2V9gGT65MjFNxNGu@tutanota.com>
2019-08-12 19:10:46 +0200

    Scripts moved from bash to sh

--- GOG Check Orphans.sh
+++ GOG Check Orphans.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 lgogdownloader --check-orphans
--- GOG.sh
+++ GOG.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 lgogdownloader --download --include all --ignore-dlc-count --no-platform-detection --platform w+l --language en+fr --save-changelogs --save-serials
